8bdde2103c353723ad7818f20315b8bea200ea71
debjyoti2019 <db.debjyotibanerjee@gmail.com>
2019-11-11 22:23:37 +0530

    added print function

--- ADA/mcolor.java
+++ ADA/mcolor.java
@@ -1,10 +1,20 @@
 import java.io.*;
 import java.util.Arrays;
 public class mcolor {
+    void print(int[] selected, int N){
+        System.out.print("[");
+        for (int i = 0; i < N ; i++) {
+            System.out.print(" "+selected[i]);
+
+        }
+        System.out.print(" ]  ");
+    }
 
     boolean solve(int vertex, int[][]array,int[]selected,int N,int m){
-        if (vertex==N)
+        if (vertex==N) {
+            print(selected, N);
             return true;
+        }
         for (int i = 1; i <= m ; i++) {
             int j;
             for (j = 0; j <N ; j++) {
@@ -17,7 +27,9 @@ public class mcolor {
             }
             if(j==N){
                 selected[vertex]=i;
+
                 if(solve(++vertex,array,selected,N,m))
+
                     return true;
                 return false;
             }
