5ce631d4d6452bf344bf81bbf98c812bed20622c
debjyoti2019 <47483452+debjyoti2019@users.noreply.github.com>
2019-11-22 15:40:18 +0530

    Update knapsack.py

--- ADA/knapsack.py
+++ ADA/knapsack.py
@@ -13,12 +13,11 @@ class Knapsack:
         """if i==-1:
             return max(self.solve(W+self.weights[i+1],i+1), self.solve(W,i+1))"""
         if W+self.weights[i+1] > self.target :
-            return 0
+            return self.solve(W,i+1)
         
         
 
         return max(self.values[i+1]+self.solve(W+self.weights[i+1],i+1), self.solve(W,i+1))
     
-k= Knapsack(0,[10],[60],50)
+k= Knapsack(1,[10,4],[60,12],5)
 print(k.solve(0,-1))
-
